b8f1b1a7f3d5c11bac28ed21f2cf04ec3b9d076b
NeverMore27 <uragan.uragan.59@mail.ru>
2017-12-06 22:07:12 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -15,22 +15,29 @@ TEST_CASE("pop")
     int s = v.pop_back();
     int z = v.pop_front();                     
     REQUIRE(s== 4);
-    REQUIRE(s.count() == 1)                  
+    REQUIRE(v.count() == 1)                  
     REQUIRE(z==1);
 }
 TEST_CASE("init list") 
 {
-    forward_list<int> s={1, 2, 3, 4, 5};
+    forward_list<int> s={1, 2, 3, 4, 5, 6};
     int s = v.pop_back();
-    REQUIRE(s.count() == 5);
+    REQUIRE(v.count() == 5);
+    REQUIRE(s == 6);
     s = v.pop_back();
-    REQUIRE(s.count() == 4);
+    REQUIRE(s == 5);
+    REQUIRE(v.count() == 4);
     s = v.pop_back();
-    REQUIRE(s.count() == 3);
+    REQUIRE(s == 4);
+    REQUIRE(v.count() == 3);
     s = v.pop_back();
-    REQUIRE(s.count() == 2);
+    REQUIRE(s == 3);
+    REQUIRE(v.count() == 2);
     s = v.pop_back();
-    REQUIRE(s.count() == 1);
+    REQUIRE(s == 2);
+    REQUIRE(v.count() == 1);
+    s = v.pop_back();
+    REQUIRE(s == 1);
     REQUIRE(!v.empty());
 }
 
