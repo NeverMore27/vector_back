309249bb4cfc65ab62f8f268b6206171bfb6a9a6
NeverMore27 <uragan.uragan.59@mail.ru>
2017-12-06 22:00:50 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -23,13 +23,13 @@ TEST_CASE("init list")
     forward_list<int> s={1, 2, 3, 4, 5};
     int s = v.pop_back();
     REQUIRE(s.count() == 5);
-     s = v.pop_back();
+    s = v.pop_back();
     REQUIRE(s.count() == 4);
-     s = v.pop_back();
+    s = v.pop_back();
     REQUIRE(s.count() == 3);
-     s = v.pop_back();
+    s = v.pop_back();
     REQUIRE(s.count() == 2);
-     s = v.pop_back();
+    s = v.pop_back();
     REQUIRE(s.count() == 1);
     REQUIRE(!v.empty());
 }
@@ -46,23 +46,23 @@ TEST_CASE("push")
 {
    forward_list<int> s = {1, 2, 5, 9};
    forward_list<int> m = {1, 2, 5, 9};
-    REQUIRE(m == s);
+   REQUIRE(m == s);
 }
  TEST_CASE("move") 
 {
    forward_list<int> s = {1, 2, 5, 9};
    forward_list<int> m = move(s);
-    REQUIRE(m == s);
+   REQUIRE(m == s);
 }
  TEST_CASE("copy") 
 {
    forward_list<int> s = {1, 2, 5, 9};
    forward_list<int> m =s;
-    REQUIRE(m == s);
+   REQUIRE(m == s);
 }
-	 TEST_CASE("move") 
+TEST_CASE("back") 
 {
-   forward_list<int> s = {1, 2, 5, 9};
+   forward_list<int> s = {1, 2, 4};
    forward_list<int> m;
    forward_list<int> m2;
    m.push_back(1);
@@ -71,7 +71,21 @@ TEST_CASE("push")
    m.emplase_back(1);
    m.emplase_back(2);
    m.emplase_back(4);
-    REQUIRE(m == s);
-    REQUIRE(m2 == s);
+   REQUIRE(m == s);
+   REQUIRE(m2 == s);
+}
+TEST_CASE("front") 
+{
+   forward_list<int> s = {4, 2, 1};
+   forward_list<int> m;
+   forward_list<int> m2;
+   m.push_front(1);
+   m.push_front(2);
+   m.push_front(4);
+   m.emplase_front(1);
+   m.emplase_front(2);
+   m.emplase_front(4);
+   REQUIRE(m == s);
+   REQUIRE(m2 == s);
 }
 
