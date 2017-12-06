ca1acab2db35290053fd195df3db6902efa4d19f
NeverMore27 <uragan.uragan.59@mail.ru>
2017-12-06 23:37:11 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -63,4 +63,39 @@ SCENARIO("clear")
    forward_list<int> m =std:: move(s);
    REQUIRE(m.count()==2);
 }
+SCENARIO("copy") 
+{
+   forward_list<int> s = {1, 2, 5, 9};
+   forward_list<int> m =s;
+   REQUIRE((m == s));
+}
+SCENARIO("back") 
+{
+   forward_list<int> s = {1, 2, 4};
+   forward_list<int> m;
+   forward_list<int> m2;
+   m.push_back(1);
+   m.push_back(2);
+   m.push_back(4);
+   m.emplace_back(1);
+   m.emplace_back(2);
+   m.emplace_back(4);
+   REQUIRE((m == s));
+   REQUIRE((m2 == s));
+}
+SCENARIO("front") 
+{
+   forward_list<int> s = {4, 2, 1};
+   forward_list<int> m;
+   forward_list<int> m2;
+   m.push_front(1);
+   m.push_front(2);
+   m.push_front(4);
+   m.emplace_front(1);
+   m.emplace_front(2);
+   m.emplace_front(4);
+   REQUIRE((m == s));
+   REQUIRE((m2 == s));
+}
+
 
